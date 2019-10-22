55b78a6d404840f79bd59f33c2545c83e04c23eb
Ryan Schaffer <ryan@tactucalcodemonkeys.com>
2019-10-22 17:14:27 -0400

    updating for step1

--- run.py
+++ run.py
@@ -1,57 +1,64 @@
 #!/bin/python3
 
 from random import randint
-  
-player = input('rock (r), paper (p) or scissors (s)?')
 
-if player == 'r':
-  print('O', end=' ')
+def play():
   
-elif player == 'p':
-  print('___', end=' ')
-  
-elif player == 's':
-  print('>8', end=' ')
-  
-else:
-  print('??')
-  
-print('vs', end=' ')
+    player = input('rock (r), paper (p) or scissors (s)?')
 
-chosen = randint(1,3)
+    if player == 'r':
+      print('O', end=' ')
+      
+    elif player == 'p':
+      print('___', end=' ')
+      
+    elif player == 's':
+      print('>8', end=' ')
+      
+    else:
+      print('??')
+      
+    print('vs', end=' ')
 
-if chosen == 1 :
-  computer = 'r'
-  print('O')
-  
-elif chosen == 2 :
-  computer = 'p'
-  print('___')
-  
-else:
-  computer = 's'
-  print('>8')
+    chosen = randint(1,3)
 
-if player == computer:
-  print('DRAW!')
-  
-elif player == 'r' and computer == 's':
-  print('Player wins!')
-  
-elif player == 'r' and computer == 'p':
-  print('Computer wins!')
-  
-elif player == 'p' and computer == 'r':
-  print('Player wins!')
-  
-elif player == 'p' and computer == 's':
-  print('Computer wins!')
+    if chosen == 1 :
+      computer = 'r'
+      print('O')
+      
+    elif chosen == 2 :
+      computer = 'p'
+      print('___')
+      
+    else:
+      computer = 's'
+      print('>8')
 
-elif player == 's' and computer == 'p':
-  print('Player wins!')
-  
-elif player == "s" and computer == 'r':
-  print('Computer wins!')
+    if player == computer:
+      print('DRAW!')
+      
+    elif player == 'r' and computer == 's':
+      print('Player wins!')
+      
+    elif player == 'r' and computer == 'p':
+      print('Computer wins!')
+      
+    elif player == 'p' and computer == 'r':
+      print('Player wins!')
+      
+    elif player == 'p' and computer == 's':
+      print('Computer wins!')
 
-else:
-  print('Huh?')
+    elif player == 's' and computer == 'p':
+      print('Player wins!')
+      
+    elif player == "s" and computer == 'r':
+      print('Computer wins!')
+
+    else:
+      print('Huh?')
+    
+
+    play()
+
+play()
